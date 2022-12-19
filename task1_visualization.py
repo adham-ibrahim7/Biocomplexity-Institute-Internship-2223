3cbaa675ab07678cfc90f23d29862d7c1cf8baed
Adham Ibrahim <37982706+adham-ibrahim7@users.noreply.github.com>
2022-12-18 22:35:10 -0500

    confidence intervals, calibration of sigma by minimizing CRPS score

--- task1_visualization.py
+++ task1_visualization.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-import statistics
+from scipy.integrate import quad as integral
 
 forecast_reader = csv.reader(open("flu_fct_processed.csv", "r"), delimiter=',')
 
@@ -27,17 +27,17 @@ T = len(dates)
 
 ensemble_parameters_reader = csv.reader(open("ensemble_parameters.csv", "r"), delimiter=",")
 
-sigma = {}
-w = {}
+sigma_from_iters = {}
+w_from_iters = {}
 
 for (i, row) in enumerate(ensemble_parameters_reader):
     if i == 0:
         continue
-    iters = int(row[0])
+    EM_iters = int(row[0])
     sigma_i = float(row[1])
     w_i = list(map(float, row[2:]))
-    sigma[iters] = sigma_i
-    w[iters] = w_i
+    sigma_from_iters[EM_iters] = sigma_i
+    w_from_iters[EM_iters] = w_i
 
 regression_parameters_reader = csv.reader(open("regression_parameters.csv", "r"), delimiter=",")
 
@@ -50,17 +50,101 @@ for (i, row) in enumerate(regression_parameters_reader):
     elif i == 2:
         b = list(map(float, row[1:]))
 
-x_axis = np.arange(2000, 18000, 50)
+################################################################################################
+# PLOT THE BMA PDF
+################################################################################################
 
-iters = 10
+def plot_pdf(sigma, w, x_axis):
+    def ensemble_pdf(curr_sigma, X):
+        pdf = 0
+        for k in range(K):
+            # print(a[k] + b[k] * forecasts[k][-1])
+            pdf += w[k] * norm.pdf(X, a[k] + b[k] * forecasts[k][-1], curr_sigma)
+        return pdf
 
-f = 0
-for k in range(K):
-    print(a[k] + b[k] * forecasts[k][-1])
-    f += w[iters][k] * norm.pdf(x_axis, forecasts[k][-1], sigma[iters])
+    plt.plot(x_axis, ensemble_pdf(sigma, x_axis))
+    plt.show()
 
-plt.plot(x_axis, f)
-plt.show()
+EM_iters = 10
+sigma = sigma_from_iters[EM_iters]
+w = w_from_iters[EM_iters]
+
+# plot_pdf(sigma, w)
+
+################################################################################################
+# GENERATE SAMPLES AND COMPUTE 50%, 95% CI
+################################################################################################
+
+def sample(sigma):
+    k = np.random.choice(list(range(K)), p=w)
+    mean = forecasts[k][-1]
+    return np.random.normal(mean, sigma)
+
+def get_CI(CI_size, sigma):
+    n_samples = 1000
+    samples = np.empty(n_samples)
+    for i in range(n_samples):
+        samples[i] = sample(sigma)
+
+    samples = sorted(samples)
+
+    start_index = int(n_samples * (.5 * (1 - CI_size)))
+    end_index = n_samples - start_index
+    return samples[start_index], samples[end_index]
+
+print("BEFORE CALIBRATION:")
+print("MIDDLE 50%:", get_CI(.5, sigma))
+print("MIDDLE 95%:", get_CI(.95, sigma))
+plot_pdf(sigma, w, np.arange(6000, 16000, 10))
+
+################################################################################################
+# CALIBRATION OF SIGMA TO MAXIMIZE CRPS SCORE
+################################################################################################
+
+def ensemble_cdf(curr_sigma, X):
+    cdf = 0
+    for k in range(K):
+        cdf += w[k] * norm.cdf(X, a[k] + b[k] * forecasts[k][-1], curr_sigma)
+    return cdf
+
+def CRPS(sigma, x):
+    cdf = lambda t: ensemble_cdf(sigma, t)
+
+    def f_1(t):
+        y = cdf(t)
+        return y * y
+
+    def f_2(t):
+        y = cdf(t)
+        return (1 - y) * (1 - y)
+
+    return integral(f_1, 0, x)[0] + integral(f_2, x, 30000)[0]
+
+# print(sigma)
+# print(CRPS(sigma, gtruth[-1]))
+
+x_axis = np.arange(0, 400, 20)
+
+lo = 0
+hi = 400
+count = 0
+while hi - lo > 1:
+    count += 1
+
+    mid_left = lo + (hi - lo) / 3
+    mid_right = lo + (hi - lo) * 2 / 3
+    if CRPS(mid_left, gtruth[-1]) < CRPS(mid_right, gtruth[-1]):
+        hi = mid_right
+    else:
+        lo = mid_left
+
+calibrated_sigma = lo
+
+print("AFTER CALIBRATION:")
+print("MIDDLE 50%:", get_CI(.5, calibrated_sigma))
+print("MIDDLE 95%:", get_CI(.95, calibrated_sigma))
+plot_pdf(lo, w, np.arange(10000, 12500, 10))
+
 #
-# print(list(forecasts[k][-1] for k in range(K)))
-# print(w[iters])
+# plt.plot(x_axis, list(map(lambda s: CRPS(s, gtruth[-1]), x_axis)))
+# plt.show()
